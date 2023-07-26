e3d7235da32ae6685fdd42ef94b1b032dc99796a
Sardor <sardoribodov07@gmail.com>
2023-07-26 11:30:19 +0500

    Intro to Javascript

---
+++ 1. Intro/script.js
@@ -0,0 +1,83 @@
+// console.log("hello js");
+
+// JS kodlarining natijasini 4 xil usulda ko'ra olamiz:
+// 1. NodeJS terminal
+// 2. Browser
+// 3. Online Compilers
+// 4. Browser console
+
+// Single thread / Multi thread
+
+// Variables
+// const / let / var variableName = variableValue;
+// const name = "Dilshod";
+// name = "Sodiq";
+// console.log(name); // error
+// const 1name = "Dilshod" // error
+
+// Data Types
+// Primitives
+// number
+// const number = 10;
+// console.log(number);
+// console.log(typeof number);
+
+// bigInt -> (-9007199254740991n yoki 9007199254740991n)
+// const bigInteger = 9007199254740992n;
+// console.log(bigInteger + 5n);
+// console.log(typeof(bigInteger + 5n));
+
+// string -> ('', "", ``)
+// const season = "Winter";
+// console.log("Winter is cold!");
+// console.log('Winter is cold!');
+// console.log(`${season} is cold!`);
+// console.log(typeof 'Winter is cold!');
+
+// boolean -> true -> 1, false -> 0
+// console.log(typeof true);
+
+// null
+// let box = null;
+// console.log(box);
+// console.log(typeof box);
+
+// undefined
+// let box;
+// console.log(box);
+// console.log(typeof box);
+
+// symbol -> for unique id
+// let id1 = Symbol("Sardor");
+// let id2 = Symbol("Nodir");
+// console.log(id1, id2);
+// console.log(typeof id1, typeof id2);
+
+// Non-Primitive -> object, array, function
+// object
+// const person = {
+// 	firstName: "Sardor",
+// 	age: 19,
+// 	weight: 80,
+// 	isMarried: false
+// }
+// console.log(person);
+// console.log(typeof person);
+
+// array
+// const array = [1,2,3,4,5,6];
+// console.log(array);
+// console.log(typeof array);
+
+// function 
+// function sayHello(name) {
+// 	return `Hello ${name}`
+// }
+
+// console.log(sayHello("Sardor"));
+// console.log(typeof sayHello);
+
+// Primitive vs Non Primitive
+// Primitive -> pass by value
+// Non Primitive -> pass by reference
+
